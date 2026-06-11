4ef58021cf6f28d7bb213bc1acac339859c3e9dc
felipinhobxd <felipinhovecoski@gmail.com>
2026-06-11 19:56:12 -0300

    feat: add leap year checker script

--- Python-100days/day-3/ano-bissexto.py
+++ Python-100days/day-3/ano-bissexto.py
@@ -1,10 +1,10 @@
 ano = int(input("Qual ano você que sabe que é bissexto?\n"))
 if ano % 4 == 0:
-    if ano % 100 == 0:
+    if ano % 100 != 0:
         print("É BISSEXTO")
     elif ano % 400 == 0:
         print("É BISSEXTO!")
     else:
-        print("NÃO É BISSEXTO!")       
+        print("NÃO É BISSEXTO!")      
 else:
     print("NÃO É BISSEXTO!")
