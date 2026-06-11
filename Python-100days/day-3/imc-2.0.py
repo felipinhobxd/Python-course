36948146a44bbfa587b2b63577f8ce211733c3b9
felipinhobxd <felipinhovecoski@gmail.com>
2026-06-11 18:51:00 -0300

    feat: display bmi category alongside the numeric result

--- Python-100days/day-3/imc-2.0.py
+++ Python-100days/day-3/imc-2.0.py
@@ -5,11 +5,11 @@ print(imc)
 
 if imc < 18.5:
     print("Você é o Jonathan!")
-elif imc >= 18.5 and imc < 25:
+elif imc < 25:
     print("Você ta normal!")
-elif imc >=25 and imc < 30:
+elif imc < 30:
     print("Você ta ligeiramente acima do peso!")
-elif imc >=30 and imc < 35:
+elif imc < 35:
     print("Você está obeso!")
 else:
     print("Você ta com obesidade morbida!")
