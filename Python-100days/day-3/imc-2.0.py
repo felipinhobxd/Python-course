25b8f918b67a9a3aadafadd207a46d671406c162
felipinhobxd <felipinhovecoski@gmail.com>
2026-06-11 18:55:54 -0300

    feat: display bmi category alongside the numeric result

--- Python-100days/day-3/imc-2.0.py
+++ Python-100days/day-3/imc-2.0.py
@@ -1,15 +1,13 @@
 altura = input("Digite sua altura em metros:\n")
 peso = input("Digite seu peso em quilos:\n")
 imc = float(peso) / float(altura) ** 2
-print(imc)
-
 if imc < 18.5:
-    print("Você é o Jonathan!")
+    print(f"seu imc é {imc}, Você é o Jonathan!")
 elif imc < 25:
-    print("Você ta normal!")
+    print(f"seu imc é {imc}, Você ta normal!")
 elif imc < 30:
-    print("Você ta ligeiramente acima do peso!")
+    print(f"seu imc é {imc}, Você ta ligeiramente acima do peso!")
 elif imc < 35:
-    print("Você está obeso!")
+    print(f"seu imc é {imc}, Você está obeso!")
 else:
-    print("Você ta com obesidade morbida!")
+    print(f"seu imc é {imc}, Você ta com obesidade morbida!")
