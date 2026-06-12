99f234f23327dcafc70244450373a27945accb4d
Felipe Veçoski <85942255+felipinhobxd@users.noreply.github.com>
2026-06-12 16:55:52 -0300

    Fix calculation of remaining weeks in life

--- Python-100days/day-2/vida-restante-90-anos.py
+++ Python-100days/day-2/vida-restante-90-anos.py
@@ -1,4 +1,4 @@
 idade = int(input("Quantos Anos você tem?\n"))
 anos = 90 - idade
 semanas = 52.14 * anos
-print(f"você tem {idade_restante} semanas de vida")
+print(f"você tem {semanas} semanas de vida")
