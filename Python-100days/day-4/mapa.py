49ea5a3a07f3f798e77fc2251c95c2bf579430f7
felipinhobxd <felipinhovecoski@gmail.com>
2026-06-15 18:36:09 -0300

    feat: add coordinate-based treasure map grid

--- Python-100days/day-4/mapa.py
+++ Python-100days/day-4/mapa.py
@@ -7,24 +7,11 @@ print("O X marca onde seu tesouro será escondido")
 print(f"\n{linha1}\n{linha2}\n{linha3}")
 
 posicao = input("Onde quer esconder seu tesouro? A1, A2, A3, B1, B2, B3, C1, C2 ou C3\n")
-posicao_real = posicao.lower()
+letra = posicao[0].lower()
+abc = ["a", "b", "c"]
+letra_index = abc.index(letra)
+numero_index = int(posicao[1]) - 1
 
-letra = posicao_real[0]
-numero = posicao_real[1]
+mapa[letra_index][numero_index] = "❌"
 
-if letra == "a":
-    coluna = 0
-elif letra == "b":
-    coluna = 1
-else:
-    coluna = 2
-
-if numero == "1":
-    linha = 0
-elif numero == "2":
-    linha = 1
-else: 
-    linha = 2
-
-mapa[linha][coluna] = "❌"
 print(f"\n{linha1}\n{linha2}\n{linha3}")
