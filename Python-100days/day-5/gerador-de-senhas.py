b26b264f0b1b50044f8c0c5c1eed2db1a8480298
felipinhobxd <felipinhovecoski@gmail.com>
2026-06-19 17:25:59 -0300

    feat: add password creator using shuffle and choice methods

--- Python-100days/day-5/gerador-de-senhas.py
+++ Python-100days/day-5/gerador-de-senhas.py
@@ -7,21 +7,21 @@ letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
 numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 simbolos = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Bem-vindo ao Gerador de Senhas em Python!")
-nr_letras = int(input(f"Quantas letras você gostaria de ter na sua senha?\n"))
-nr_simbolos = int(input(f"Quantos símbolos você gostaria de ter?\n"))
-nr_numeros = int(input(f"Quantos números você gostaria de ter?\n"))
+print("Bem-vindo ao Gerador de Senhas em Python!😭")
+nr_letras = int(input(f"Quantas letras você gostaria de ter na sua senha?🔠\n"))
+nr_simbolos = int(input(f"Quantos símbolos você gostaria de ter?🔣\n"))
+nr_numeros = int(input(f"Quantos números você gostaria de ter?🔢\n"))
 
 lista_senha = []
 for i in range(nr_letras):
     letras_aleatorias = random.choice(letras)
     lista_senha.append(letras_aleatorias)
 for i in range(nr_numeros):
-    numeros_aleatorios = random.choice(numeros)
-    lista_senha.append(numeros_aleatorios)
-for i in range(nr_simbolos):
     simbolos_aleatorios = random.choice(simbolos)
     lista_senha.append(simbolos_aleatorios)
+for i in range(nr_simbolos):
+    numeros_aleatorios = random.choice(numeros)
+    lista_senha.append(numeros_aleatorios)
 random.shuffle(lista_senha)
 senha_final = "".join(lista_senha)
 print(f"A SUA SENHA NOVA É: {senha_final}")
